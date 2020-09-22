0428c1a28c8a94a77a526f88f18f48f9b74497fe
Vasyl Tykhan <tykhan@MacBook-Pro-Vasyl.local>
2020-09-22 18:31:52 +0300

    Updates

---
+++ precache-manifest.4803e7200418d3af777c210127eab2d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2bfcf7bc56cce3735735c8b10699663",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "226b0adbae57fadc722a",
+    "url": "/todo-app/static/css/main.7527946b.chunk.css"
+  },
+  {
+    "revision": "13e7b2ae8e54640c828d",
+    "url": "/todo-app/static/js/2.5225fe09.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-app/static/js/2.5225fe09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226b0adbae57fadc722a",
+    "url": "/todo-app/static/js/main.d827b5d9.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
